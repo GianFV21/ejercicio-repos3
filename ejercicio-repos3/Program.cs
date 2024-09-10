0881ebe540fe173af28cb7f6fde8c9102fc95b5f
Flores V�squez Carlos Gianfranco <FV23003@ues.edu.sv>
2024-09-10 17:04:37 -0600

    elememto multi

--- ejercicio-repos3/Program.cs
+++ ejercicio-repos3/Program.cs
@@ -11,13 +11,15 @@ namespace ejercicio_repos3
         static void Main(string[] args)
         {
             Double Num1, Num2;
-            Double Suma = 0;
+            Double Suma = 0, Multi = 0;
             Console.WriteLine("Ingrese el numero 1: ");
             Num1 = Double.Parse(Console.ReadLine());
             Console.WriteLine("Ingrese el numero 2: ");
             Num2 = Double.Parse(Console.ReadLine());
             Suma = Num1 + Num2;
-            Console.WriteLine("El resultado de la suma es de: ");
+            Multi = Num1 * Num2;
+            Console.WriteLine("El resultado de la suma es de:{0} ",Suma);
+            Console.WriteLine("El resultado de la Multiplicacion es: {1}", Multi);
             Console.ReadKey();
         }
     }

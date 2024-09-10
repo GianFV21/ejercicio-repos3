924675f0187d85987dc04753b23e45ce41d5df4a
Flores V�squez Carlos Gianfranco <FV23003@ues.edu.sv>
2024-09-10 16:58:54 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio-repos3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_repos3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Num1, Num2;
+            Double Suma = 0;
+            Console.WriteLine("Ingrese el numero 1: ");
+            Num1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero 2: ");
+            Num2 = Double.Parse(Console.ReadLine());
+            Suma = Num1 + Num2;
+            Console.WriteLine("El resultado de la suma es de: ");
+            Console.ReadKey();
+        }
+    }
+}
